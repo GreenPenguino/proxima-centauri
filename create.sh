c177358b353ba82ed0f4731499ba31dfc02b6fbb
Erik <erik_97@live.nl>
2023-04-18 11:08:54 +0200

    Update example scripts

--- create.sh
+++ create.sh
@@ -1,12 +1,10 @@
 curl --header "Content-Type: application/json" \
-  --data '{"command": {
-                       "Create": {
-                                "incoming_port": 5555,
-                                "destination_port": 8080,
-                                "destination_ip": "127.0.0.1",
-                                "id": "67e55044-10b1-426f-9247-bb680e5fe0c8"
-                              }
-                     },
-           "signature": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-         }' \
+  --data '{
+            "create": {
+                    "incoming_port": 5555,
+                    "destination_port": 8080,
+                    "destination_ip": "127.0.0.1",
+                    "id": "67e55044-10b1-426f-9247-bb680e5fe0c8"
+            }
+          }' \
   http://localhost:3000/command
