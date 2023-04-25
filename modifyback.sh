f3c20341fa999b71b16f163622b2519f135202ec
Erik <erik_97@live.nl>
2023-04-25 12:55:53 +0200

    Add modifyback script

---
+++ modifyback.sh
@@ -0,0 +1,9 @@
+curl --header "Content-Type: application/json" \
+  --data '{
+            "modify": {
+                    "destination_port": 8080,
+                    "destination_ip": "127.0.0.1",
+                    "id": "67e55044-10b1-426f-9247-bb680e5fe0c8"
+                  }
+          }' \
+  http://localhost:3000/command
